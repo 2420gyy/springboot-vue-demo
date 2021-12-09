01b3a7b94bd8cc9910264c7c4e445f8fec14fa26
2420gyy <2420191325@qq.com>
2021-12-09 22:45:30 +0800

    添加springboot更改项目结构

---
+++ springbootManageSystem/src/main/java/com/chen/managesystem/SpringbootManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.chen.managesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootManageSystemApplication.class, args);
+    }
+
+}
---
+++ springbootManageSystem/src/test/java/com/chen/managesystem/SpringbootManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.managesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
